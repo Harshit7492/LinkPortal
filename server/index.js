2ba3b7f43f9555c60aef62a053fcadf4caec478c
Harshit <harshit28k@gmail.com>
2024-09-27 12:02:05 +0530

    new Mongo URI Updated

--- server/index.js
+++ server/index.js
@@ -14,13 +14,13 @@ app.use(cors());
 app.use(express.json())
 
 
-mongoose.connect('mongodb://127.0.0.1:27017/linktree')
-.then(()=>{
-    console.log("Mongodb connected successfuly")
-}).catch(err=>{
-    console.log("Mongodb not connected")
-})
-
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('MongoDB connected successfully');
+  })
+  .catch((err) => {
+    console.error('MongoDB connection error:', err);
+  });
 app.post('/api/register',registerUser)
 app.post('/api/login',loginUser)
 app.post('/data/dashboard',dashboardData)
